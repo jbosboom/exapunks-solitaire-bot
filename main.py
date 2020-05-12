6805b819ef161c12de1797d863e22164bf90669e
Jeffrey Bosboom <jeffreybosboom@gmail.com>
2020-05-12 16:09:57 -0700

    Solver
    I don't have enough memory on my dev machine to actually solve a puzzle,
    so there are probably still bugs lurking here.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import namedtuple, deque
 from pathlib import Path
 import pyautogui
 from recognizer import Recognizer
@@ -24,6 +24,109 @@ def parse_screenshot(image, recognizer):
     stacks = tuple(stacks)
     return Puzzle(stacks, None)
 
+_suit_stacks = [(suit,)*4 for suit in ('club', 'diamond', 'heart', 'spade')]
+_pip_stacks = [
+    (('black', 10), ('red', 9), ('black', 8), ('red', 7), ('black', 6)),
+    (('red', 10), ('black', 9), ('red', 8), ('black', 7), ('red', 6)),
+]
+def is_solved(puzzle: Puzzle):
+    if puzzle.free: return False
+    for suit_stack in _suit_stacks:
+        try:
+            puzzle.stacks.index(suit_stack)
+        except ValueError:
+            return False
+    for pip_stack in _pip_stacks:
+        try:
+            i = puzzle.stacks.index(pip_stack)
+            puzzle.stacks.index(pip_stack, i+1)
+        except ValueError:
+            return False
+    return True
+
+def stacks_onto(before, after):
+    if isinstance(before, str) or isinstance(after, str):
+        return before == after
+    sb, nb = before
+    sa, na = after
+    return sb != sa and nb == (na + 1)
+
+def maximal_stack(stack):
+    m = len(stack)-1
+    while m > 0:
+        if stacks_onto(stack[m-1], stack[m]):
+            m -= 1
+        else:
+            break
+    return m
+
+class MaxStackCache(dict):
+    def __missing__(self, key):
+        return maximal_stack(key)
+
+def solve(puzzle: Puzzle):
+    parent = {} # maps puzzles to (puzzle, move descriptor) pairs
+    pending = deque()
+    max_stack = MaxStackCache()
+    solved = None
+    pending.append(puzzle)
+    while pending and not solved:
+        cur = pending.popleft()
+        for i, source in enumerate(cur.stacks):
+            if not source: continue
+            # m = max_stack[source]
+            m = maximal_stack(source)
+            chunk = source[m:]
+
+            # move this chunk to another stack
+            for j, target in enumerate(cur.stacks):
+                if i == j: continue
+                if not target or stacks_onto(target[-1], chunk[0]):
+                    stacks = list(cur.stacks)
+                    stacks[i] = stacks[i][:m]
+                    stacks[j] = stacks[j] + chunk
+                    next = Puzzle(tuple(stacks), cur.free)
+                    if next not in parent:
+                        pending.append(next)
+                        move_desc = ((i, m), (j, len(cur.stacks[j])))
+                        parent[next] = (cur, move_desc)
+                        if is_solved(next):
+                            solved = next
+
+            # if the free cell is empty and this stack is a single card, move it there
+            if not cur.free and len(chunk) == 1:
+                stacks = list(cur.stacks)
+                stacks[i] = stacks[i][:m]
+                next = Puzzle(tuple(stacks), chunk[0])
+                if next not in parent:
+                    pending.append(next)
+                    move_desc = ((i, m), 'free')
+                    parent[next] = (cur, move_desc)
+                    if is_solved(next):
+                        solved = next
+
+        # if the free cell is full, try to move it
+        if cur.free:
+            for j, target in enumerate(cur.stacks):
+                if not target or stacks_onto(target[-1], cur.free):
+                    stacks = list(cur.stacks)
+                    stacks[j] = stacks[j] + (cur.free,)
+                    next = Puzzle(tuple(stacks), None)
+                    if next not in parent:
+                        pending.append(next)
+                        move_desc = ('free', (j, len(cur.stacks[j])))
+                        parent[next] = (cur, move_desc)
+                        if is_solved(next):
+                            solved = next
+
+    solution = []
+    while backptr := parent.get(solved):
+        prev, move_desc = backptr
+        solution.append(move_desc)
+        solved = prev
+    solution.reverse()
+    return solution
+
 def main(args):
     recognizer = Recognizer(Path('data/'))
 
@@ -32,6 +135,9 @@ def main(args):
     puzzle = parse_screenshot(screenshot, recognizer)
     for stack in puzzle.stacks:
         print(stack)
+    solution = solve(puzzle)
+    print(len(solution))
+    print(solution)
 
 if __name__ == '__main__':
     main(None)
